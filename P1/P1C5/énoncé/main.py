99a5cc54c16e602561e1163c90f1fa5cddad2d00
nitresor <158075534+nitresor@users.noreply.github.com>
2026-01-02 17:17:02 +0100

    affichage_projet00

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+nom = "Nishimwe"
+age = 21
+taille = 180.9
+est_etudiant = True
+print(f"Nom:  {nom} \n  Age:  {age} \n Taille: {taille} \n Est_étudiant: {est_etudiant} " )
+print(f"Nom:  {type(nom )} \n  Age:  {type(age)} \n Taille: {type(taille)} \n Est_étudiant: {type(est_etudiant)} " )
