1a7dd46b2876378cc7b2e1d0d00f8c80d554b1d5
nitresor <158075534+nitresor@users.noreply.github.com>
2026-01-04 21:42:47 +0100

    Implement fruit list operations in main.py
    Initialize a list of fruits and perform various operations including adding, removing, and sorting the list before printing it.

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+ fruits["pomme", "banane", "orange"]
+   fruits.append("kiwi")
+   fruits.remove("orange")
+   fruits.insert(1, "ananas")
+   len(fruits)
+#trie par ordre alphabétique de la liste fruits
+   fruits.sort()
+#affichage de la liste fruits
+   print(fruits)
+   
